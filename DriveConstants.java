f9483dac793d9c3eb745905624de4de97805cf71
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2021-10-31 10:27:48 -0500

    10/29/21
    Changed max velo to the calculated value.

--- DriveConstants.java
+++ DriveConstants.java
@@ -87,7 +87,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 38.110287416570166;  //Calc max velo: 44.83563225478843, noah's mx velo: 38.110287416570166
+    public static double MAX_VEL = 44.83563225478843;  //Calc max velo: 44.83563225478843, noah's mx velo: 38.110287416570166
     public static double MAX_ACCEL = 38.110287416570166;
     public static double MAX_ANG_VEL = Math.toRadians(138.19991297468354);
     public static double MAX_ANG_ACCEL = Math.toRadians(138.19991297468354);
@@ -105,4 +105,5 @@ public class DriveConstants {
         // see https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
         return 32767 / ticksPerSecond;
     }
+
 }
