31b8bb15369aa67a7f4490fef6ee3c218524155c
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2021-11-01 09:57:33 -0500

    10/31
    Added Drive Velo PIDF values. Created a spot for F value and commented out getMotorVelocityF (Noah didn't have this in the video).

--- DriveConstants.java
+++ DriveConstants.java
@@ -34,9 +34,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(12, 0, 5,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
-
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(12, 0, 5, 13.5);
+        //getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
     /*
      * These are physical constants that can be determined from your robot (including the track
      * width; it will be tune empirically later although a rough estimate is important). Users are
